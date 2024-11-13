f4ef103a9129275081c4982e3998de5f4d3d875c
Panashe Mapika <mapikapnsh@gmail.com>
2024-11-13 22:28:24 +0200

    Use next-current-url package

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,31 +1,17 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import { useUrl } from "nextjs-current-url";
 
 const SuccessPage = () => {
-  const [subdomain, setSubdomain] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Ensure this code only runs in the browser
-    if (typeof window !== 'undefined') {
-      const hostname = window.location.hostname; // Get the hostname
-      const parts = hostname.split('.'); // Split hostname by '.'
-
-      // If there are at least three parts, we assume the first is the subdomain
-      if (parts.length >= 3) {
-        setSubdomain(parts[0]); // Set the first part as subdomain
-      } else {
-        setSubdomain(null); // No subdomain detected
-      }
-    }
-  }, []); // Run only once on mount
+  const { href: currentUrl, pathname } = useUrl() ?? {};
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Domain Configuration Successful</h1>
-      {subdomain ? (
+      {currentUrl ? (
         <p>
-          Domain <strong>{subdomain}.oono.store</strong> is successfully configured.
+          Domain <strong>{currentUrl}.oono.store</strong> is successfully
+          configured.
         </p>
       ) : (
         <p>No subdomain detected because of window.</p>
